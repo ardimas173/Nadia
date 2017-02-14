e6f7b50330fa565a176084b40248c3505f26f9a3
amud <amuddoank405@gmail.com>
2017-02-14 11:24:25 +0700

    2

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=edusec5',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+];
